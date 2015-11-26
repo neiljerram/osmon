efeedac5f152333b5fa5fddbe16bdb8c17bfdce0
Neil Jerram <Neil.Jerram@metaswitch.com>
2015-11-26 21:15:04 +0000

    First version of monitoring shell script

---
+++ osmon.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set - `openstack catalog list --format csv | gawk '{ if (match($0, "publicURL: http://[^:]*:([^/]*)/", arr)) { print arr[1]; } }' | sort | uniq`
+
+display_filter_for_port () {
+    echo "-d tcp.port==$1,http"
+}
+
+capture_filter_for_port() {
+    echo "tcp port $1"
+}
+
+display_filters=`display_filter_for_port $1`
+capture_filters=`capture_filter_for_port $1`
+shift
+
+for port in $*; do
+    display_filters="$display_filters `display_filter_for_port $port`"
+    capture_filters="$capture_filters or `capture_filter_for_port $port`"
+done
+
+sudo tshark -i any $display_filters -f "$capture_filters" -V -Y http | awk '
+
+BEGIN {
+        disp = 0;
+}
+
+/^[A-Z]/ {
+        disp = 0;
+}
+
+/^Hypertext/ {
+        disp = 1;
+}
+
+/^Java/ {
+        disp = 1;
+}
+
+{
+        if (disp) print;
+}
+'
